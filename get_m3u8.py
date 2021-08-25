5e5d83e9aea4558596165d4455666e9a611a00ed
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-08-25 22:56:01 +0800

    Create get_m3u8.py

---
+++ get_m3u8.py
@@ -0,0 +1,16 @@
+import lk21
+import sys
+
+
+def sbembed(link: str) -> str:
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_sbembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count - 1]
+
+
+link = sbembed(sys.argv[1])
+print(link)
