35c18eabe2d0bf404f73ab3b10408de99ffd105b
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-08-26 08:26:46 +0800

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -3,7 +3,80 @@ import lk21
 import sys
 
 
+def direct_link_generator(link: str):
+    if 'hxfile.co' in link:
+        return hxfile(link)
+    elif 'anonfiles.com' in link:
+        return anonfiles(link)
+    elif 'fembed.net' in link:
+        return fembed(link)
+    elif 'fembed.com' in link:
+        return fembed(link)
+    elif 'asianclub.tv' in link:
+        return fembed(link)
+    elif 'femax20.com' in link:
+        return fembed(link)
+    elif 'fcdn.stream' in link:
+        return fembed(link)
+    elif 'feurl.com' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.in' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.biz' in link:
+        return fembed(link)
+    elif 'naniplay.com' in link:
+        return fembed(link)
+    elif 'layarkacaxxi.icu' in link:
+        return fembed(link)
+    elif 'sbembed.com' in link:
+        return sbembed(link)
+    elif 'streamsb.net' in link:
+        return sbembed(link)
+    elif 'sbplay.org' in link:
+        return sbembed(link)
+    elif 'streamtape.com' in link:
+        return streamtape(link)
+    elif 'bayfiles.com' in link:
+        return anonfiles(link)
+    else:
+        return link
+
+
+def hxfile(url: str) -> str:
+    """ Hxfile direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_filesIm(url)
+    return dl_url
+
+
+def anonfiles(url: str) -> str:
+    """ Anonfiles direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_anonfiles(url)
+    return dl_url
+
+
+def fembed(link: str) -> str:
+    """ Fembed direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_fembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count - 1]
+
+
 def sbembed(link: str) -> str:
+    """ Sbembed direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
     bypasser = lk21.Bypass()
     dl_url = bypasser.bypass_sbembed(link)
     lst_link = []
@@ -13,5 +86,14 @@ def sbembed(link: str) -> str:
     return lst_link[count - 1]
 
 
-link = sbembed(sys.argv[1])
+def streamtape(url: str) -> str:
+    """ Streamtape direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_streamtape(url)
+    return dl_url
+
+
+link = direct_link_generator(sys.argv[1])
 print(link)
