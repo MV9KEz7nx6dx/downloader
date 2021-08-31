23a5bf0b79c0d79ad538d1041fd37dc3de661d4b
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-08-31 14:11:10 +0800

    Update save.py

--- save.py
+++ save.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
 parser.add_argument("--con", help="Connection url", default="")
 parser.add_argument("--name", help="Insert name", default="")
 parser.add_argument("--cid", help="Insert cid", default="")
+parser.add_argument("--size", help="Insert size", default="")
 
 args = parser.parse_args()
 
@@ -16,6 +17,6 @@ client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
 mydb = client["mydb"]
 mycol = mydb["web3"]
 
-mydict = {"name":args.name,"cid": args.cid,"issync":"0"}
+mydict = {"name":args.name,"cid": args.cid,"size": args.size,"issync":"0"}
 x = mycol.insert_one(mydict)
 print(x)
