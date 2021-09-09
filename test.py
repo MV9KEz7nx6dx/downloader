366047afb2b09c77481638b38af22bef65cf161a
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-09-09 09:03:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -41,6 +41,13 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+
+
+driver.get("https://www.baidu.com/")
+print(driver.title)
+quit()
+
+
 driver.get(url)
 videos=driver.find_elements_by_css_selector('a.thumb')
 links=[]
