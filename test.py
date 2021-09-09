c686352a9edf0288d4cafe32bc096b0eaadd19ff
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-09-09 17:26:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -69,7 +69,8 @@ for index,link in enumerate(links):
     driver.implicitly_wait(2)
     windows = driver.window_handles
     driver.switch_to.window(windows[1])
-    button=wait.until(EC.element_to_be_clickable((By.XPATH,  '//button[contains(text(),"DOWNLOAD")]')))
+#    button=wait.until(EC.element_to_be_clickable((By.XPATH,  '//button[contains(text(),"DOWNLOAD")]')))
+    button=driver.find_element(By.XPATH, '//button[contains(text(),"DOWNLOAD")]')
     ActionChains(driver).click(button).perform()
     driver.implicitly_wait(2)
     windows = driver.window_handles
