6fab2a45e353d0ded7ed4ddddf97682c3ed89f12
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-10-28 09:25:28 +0800

    Create movie.py

---
+++ movie.py
@@ -0,0 +1,27 @@
+import certifi
+import pymongo
+import sys
+import argparse
+from bson.json_util import dumps, loads
+
+parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
+parser.add_argument("--con", help="Connection url", default="")
+
+
+
+args = parser.parse_args()
+
+
+client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
+mydb = client["mydb"]
+mycol = mydb["mtask"]
+
+
+x = mycol.find_one({})
+if x is None:
+    print("None")
+    quit()
+
+info = loads(dumps(x))
+mycol.delete_one(x)
+print(info['url'])
