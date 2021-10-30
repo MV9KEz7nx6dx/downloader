2cec21b74e7fcef375c46c8a92749657369aef57
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2021-10-30 18:54:53 +0800

    Update download.py

--- download.py
+++ download.py
@@ -6,26 +6,26 @@ from bson.json_util import dumps, loads
 import os
 
 parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
-parser.add_argument("--con", help="Connection url", default="")
+parser.add_argument("--url", help="Download url", default="")
+parser.add_argument("--filename", help="Download url", default="")
 
 
 
 args = parser.parse_args()
 
 
-client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
-mydb = client["mydb"]
-mycol = mydb["task"]
+# client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
+# mydb = client["mydb"]
+# mycol = mydb["task"]
 
 
-x = mycol.find_one({})
-if x is None:
-    print("None")
-    quit()
+# x = mycol.find_one({})
+# if x is None:
+#     print("None")
+#     quit()
 
-info = loads(dumps(x))
-mycol.delete_one(x)
-urlinfo = info['url'].split("##");
-cmd = "aria2c --conf aria2.conf --seed-time=0 -o "+urlinfo[1]+" -d downloads -c \""+urlinfo[0]+"\""
+# info = loads(dumps(x))
+# mycol.delete_one(x)
+# urlinfo = info['url'].split("##");
+cmd = "aria2c --conf aria2.conf --seed-time=0 -o "+args.filename+" -d downloads -c \""+args.url+"\""
 os.system(cmd)
-print(urlinfo[1])
