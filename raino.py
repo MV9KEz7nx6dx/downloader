576ea7ba04a6c7057ff3a3330c9c3a7e89da39ef
xdrghbjiikmdghu <4392663+xdrghbjiikmdghu@users.noreply.github.com>
2022-06-22 19:03:26 +0800

    Update raino.py

--- raino.py
+++ raino.py
@@ -8,27 +8,28 @@ import argparse
 from bson.json_util import dumps, loads
 
 
-parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
-parser.add_argument("--con", help="Connection url", default="")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
+# parser.add_argument("--con", help="Connection url", default="")
+# args = parser.parse_args()
 
 
-client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
-mydb = client["mydb"]
-mycol = mydb["setting"]
+# client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
+# mydb = client["mydb"]
+# mycol = mydb["setting"]
 
 
 
-r = requests.get('https://raino.dev/30pikpak')
-username_regex = r"账号\<span class=\"notion-orange\"\>([^\<]*)\<\/span\>"
-username_pattern = re.search(username_regex, r.text)
-if username_pattern:
-	username = username_pattern.group(1)
-	#print(username_pattern.group(1))
-else:
-	print("找不到用户名")
-	quit()
+# r = requests.get('https://raino.dev/30pikpak')
+# username_regex = r"账号\<span class=\"notion-orange\"\>([^\<]*)\<\/span\>"
+# username_pattern = re.search(username_regex, r.text)
+# if username_pattern:
+# 	username = username_pattern.group(1)
+# 	#print(username_pattern.group(1))
+# else:
+# 	print("找不到用户名")
+# 	quit()
 
+username="hhello@123"
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 if(re.fullmatch(regex, username)):
@@ -36,6 +37,9 @@ if(re.fullmatch(regex, username)):
 else:
 	print("Invalid Email")
 	#quit()
+
+	
+quit()
 
 password_regex = r"密码\<span class=\"notion-orange\"\>([^\<]*)\<\/span\>"
 password_pattern = re.search(password_regex, r.text)
