994394d4640817ea4e790fa0a301e3420dec9e26
MV9KEz7nx6dx <4392663+MV9KEz7nx6dx@users.noreply.github.com>
2022-09-15 19:00:49 +0800

    Update task.py

--- task.py
+++ task.py
@@ -16,7 +16,7 @@ client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
 mydb = client["mydb"]
 mycol = mydb["task"]
 
-x = mycol.find_one_and_update({'isnow': args.isnow}, '$set': {'isnow': 10}})
+x = mycol.find_one_and_update({'isnow': args.isnow}, '$set': {'isnow': 10})
 if x is None:
     print("None")
     quit()
