6da0c689bc8ca2867dd1ba07f83b503604d38124
MV9KEz7nx6dx <4392663+MV9KEz7nx6dx@users.noreply.github.com>
2023-05-12 08:29:15 +0800

    Create transfer.py

---
+++ transfer.py
@@ -0,0 +1,131 @@
+import certifi
+import pymongo
+import sys
+import argparse
+from bson.json_util import dumps, loads
+import os
+import lk21
+
+
+parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
+parser.add_argument("--con", help="Connection url", default="")
+parser.add_argument("--isnow",type=int, help="execute now", default=0)
+
+
+
+args = parser.parse_args()
+
+
+
+
+def direct_link_generator(link: str):
+    if 'hxfile.co' in link:
+        return hxfile(link)
+    elif 'anonfiles.com' in link:
+        return anonfiles(link)
+    elif 'fembed.net' in link:
+        return fembed(link)
+    elif 'fembed.com' in link:
+        return fembed(link)
+    elif 'asianclub.tv' in link:
+        return fembed(link)
+    elif 'dutrag.com' in link:
+        return fembed(link)
+    elif 'femax20.com' in link:
+        return fembed(link)
+    elif 'fcdn.stream' in link:
+        return fembed(link)
+    elif 'feurl.com' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.in' in link:
+        return fembed(link)
+    elif 'naniplay.nanime.biz' in link:
+        return fembed(link)
+    elif 'naniplay.com' in link:
+        return fembed(link)
+    elif 'layarkacaxxi.icu' in link:
+        return fembed(link)
+    elif 'sbembed.com' in link:
+        return sbembed(link)
+    elif 'streamsb.net' in link:
+        return sbembed(link)
+    elif 'sbplay.org' in link:
+        return sbembed(link)
+    elif 'streamtape.com' in link:
+        return streamtape(link)
+    elif 'bayfiles.com' in link:
+        return anonfiles(link)
+    else:
+        return link
+
+
+def hxfile(url: str) -> str:
+    """ Hxfile direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_filesIm(url)
+    return dl_url
+
+
+def anonfiles(url: str) -> str:
+    """ Anonfiles direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_anonfiles(url)
+    return dl_url
+
+
+def fembed(link: str) -> str:
+    """ Fembed direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_fembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count - 1]
+
+
+def sbembed(link: str) -> str:
+    """ Sbembed direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_sbembed(link)
+    lst_link = []
+    count = len(dl_url)
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+    return lst_link[count - 1]
+
+
+def streamtape(url: str) -> str:
+    """ Streamtape direct link generator
+    Based on https://github.com/zevtyardt/lk21
+             https://github.com/SlamDevs/slam-mirrorbot """
+    bypasser = lk21.Bypass()
+    dl_url = bypasser.bypass_streamtape(url)
+    return dl_url
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
+    mydb = client["mydb"]
+    mycol = mydb["transfer"]
+
+
+    #x = mycol.find_one({"isnow": args.isnow})
+    x = mycol.find_one_and_update({'isnow': args.isnow}, {'$set': {'isnow': 10}})
+    if x is None:
+        print("None")
+        quit()
+
+    info = loads(dumps(x))
+    #mycol.delete_one(x)
+    urlinfo = info['url'].split("##");
+    streamurl = direct_link_generator(urlinfo[0])
+    cmd = "aria2c --conf aria2.conf --seed-time=0 -o "+urlinfo[1]+" -d downloads -c \""+streamurl+"\""
+    os.system(cmd)
