2f3b03c948b0505c05233345ae15c38b4db5590c
MV9KEz7nx6dx <4392663+MV9KEz7nx6dx@users.noreply.github.com>
2023-06-25 21:40:24 +0800

    Create save-swarm.py

---
+++ save-swarm.py
@@ -0,0 +1,33 @@
+import certifi
+import pymongo
+import sys
+import argparse
+from bson.json_util import dumps, loads
+
+parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
+parser.add_argument("--con", help="Connection url", default="")
+parser.add_argument("--name", help="Insert name", default="")
+parser.add_argument("--cid", help="Insert cid", default="")
+parser.add_argument("--size", help="Insert size", default="")
+
+args = parser.parse_args()
+
+if args.cid is None:
+  print("Nothing to save")
+  quit()
+
+
+client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
+mydb = client["mydb"]
+mycol = mydb["swarm"]
+taskcol = mydb["task"]
+
+
+mydict = {"name":args.name,"cid": args.cid,"size": args.size,"issync":0}
+x = mycol.insert_one(mydict)
+
+keyword="##"+args.name
+task = taskcol.find_one({'url':{'$regex':keyword}})
+taskcol.delete_one(task)
+
+print(x)
