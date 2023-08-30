62028d334fab8571c90bcb621a9310da6e4b92d9
MV9KEz7nx6dx <4392663+MV9KEz7nx6dx@users.noreply.github.com>
2023-08-30 08:10:41 +0800

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -123,6 +123,12 @@ if __name__ == '__main__':
         print("None")
         quit()
 
+    urlinfo = info['url'].split("##");
+    streamurl = direct_link_generator(urlinfo[0])
+    cmd = "aria2c --conf aria2.conf --seed-time=0 -o "+urlinfo[1]+" -d downloads -c \""+streamurl+"\""
+    os.system(cmd)
+    quit()
+
     info = loads(dumps(x))
     urlinfo = info['url'].split("##");
     streamurl = direct_link_generator(urlinfo[0])
