ef1d65f8f1b5a1bb2c282e5338f66eb5eed4ce2b
MV9KEz7nx6dx <4392663+MV9KEz7nx6dx@users.noreply.github.com>
2023-09-05 08:28:56 +0800

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
     urlinfo = info['url'].split("##");
     streamurl = direct_link_generator(urlinfo[0])
     cmd = "ipfs get -o downloads "+streamurl
-    if streamurl.startswith("htttp"):
+    if streamurl.startswith("http"):
         cmd = "aria2c --conf aria2.conf --seed-time=0 -o "+urlinfo[1]+" -d downloads -c \""+streamurl+"\""
     os.system(cmd)
     quit()
